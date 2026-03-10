f57fac6091893c6205f7e99c5653bc8eaded0b8a
Nolhan <nolhan.marteau@gmail.com>
2026-03-10 14:51:30 +0100

    feat: add header component and integrate into main layout

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="header header__navbar">
+        <a href="#" class="header__navbar__logo">EasyLecture</a>
+        <ul class="header__navbar">
+            <li><a href="#" class="header__navbar-link">Boutique</a></li>
+            <li><a href="#" class="header__navbar-link">Contact FAQ</a></li>
+            <li><a href="#" class="header__navbar-link">Login</a></li>
+            <li><a href="#" class="button button--primary">Sign Up</a></li>
+        </ul>
+    </div>
+</nav>
