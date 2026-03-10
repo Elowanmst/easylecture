be344f7c8a37c6b809c8c864ffceb09475668959
Elowan MESTRES <elowanmestres@gmail.com>
2026-03-10 18:04:52 +0100

    feat: add welcome component with layout and styles, including image integration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,20 +2,23 @@
 
 @section("content")
     <div class="container">
-        <h1><span class="text-highlight">READING</span> JUST GOT EASIER</h1>
-        <p>Accède à ta bibliothèque numérique en un clic. 
-            Découvre des centaines d’<span class="text-highlight">ebooks</span> organisés par genre, 
-            trouve rapidement ce qui t’intéresse, et lis où tu veux, 
-            quand tu veux. Plus de recherches interminables, plus de 
-            complications — juste toi, un livre, et une expérience fluide 
-            pensée pour les lecteurs modernes.
-        </p>
+        <div class="welcome">
+            <div class="welcome__text">
+                <h1><span class="text-highlight">READING</span> JUST GOT EASIER</h1>
+                <p>Accède à ta bibliothèque numérique en un clic. 
+                    Découvre des centaines d'<span class="text-highlight">ebooks</span> organisés par genre, 
+                    trouve rapidement ce qui t'intéresse, et lis où tu veux, 
+                    quand tu veux. Plus de recherches interminables, plus de 
+                    complications — juste toi, un livre, et une expérience fluide 
+                    pensée pour les lecteurs modernes.
+                </p>
+                <button type="submit" class="button button--primary">Commencer à acheter</button>
+                <button type="submit" class="button button--secondary">Creer son compte</button>
+            </div>
 
-        <button type="submit" class="button button--primary">Commencer à acheter</button>
-        <button type="submit" class="button button--secondary">Creer son compte</button>
-
-
-
-
+            <div class="welcome__image">
+                <img src="{{ asset('images/lectrice.png') }}" alt="Image de lecture">
+            </div>
+        </div>
     </div>
 @endsection
