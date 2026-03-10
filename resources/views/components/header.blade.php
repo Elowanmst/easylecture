77a93073abd005d77360c337a1547d66ca961b4b
Nolhan <nolhan.marteau@gmail.com>
2026-03-10 18:04:06 +0100

    feat: update button styles and add new size variant; enhance header navigation for better responsiveness

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,7 @@
                 <li><a href="#" class="header__nav-link">Boutique</a></li>
                 <li><a href="#" class="header__nav-link">Contact FAQ</a></li>
                 <li><a href="#" class="header__nav-link">Login</a></li>
-                <li><a href="#" class="button button--primary">Sign Up</a></li>
+                <li><a href="#" class="button button--primary button--big">Sign Up</a></li>
             </ul>
         </nav>
     </div>
