03ba94895b4fed48207db46a77966126966a9758
Nolhan <nolhan.marteau@gmail.com>
2026-03-11 16:31:04 +0100

    refactor: uncomment meta tags for improved SEO and social sharing

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,14 +5,19 @@
         
         <section class="container">
             <h1 class="product__title">livre 1</h1>
+            {{-- <meta property="og:title" class="product__title" content="livre 1" /> --}}
             <article class="product__description">
                 <div class="product__image">
                     <img src="{{ asset('images/livre-test.jpg') }}">
+                    {{-- <meta property="og:image" class="product__image" content="{{ asset('images/livre-test.jpg') }}" /> --}}
                 </div>
                 <div class="card">
                     <p class="product__description">c'est la description </p>
+                    {{-- <meta property="og:description" class="product__description" content="c'est la description" /> --}}
                     <hr>
                     <p class="product__price">20€</p>
+                    {{-- <meta property="og:price:amount" class="product__price" content="20" />
+                    <meta property="og:price:currency" content="EUR" /> --}}
                     <button type="submit" class="button button--primary button__card">Ajouter au panier</button>
                 </div>
             </article>
