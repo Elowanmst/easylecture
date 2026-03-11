b5deb9b2b84930d09c0e8c1cd403c817fbc89551
Nolhan <nolhan.marteau@gmail.com>
2026-03-11 09:35:26 +0100

    feat: enhance product card layout and styles; add image handling with hover effect

--- resources/views/components/card-product.blade.php
+++ resources/views/components/card-product.blade.php
@@ -1,5 +1,10 @@
 <article class="card product-card">
-    <h3 class="product-card__title">Titre du Livre 1</h3>
-    <p class="product-card__price">€9.99</p>
-    <a href="#" class="button button--primary button__card">Ajouter au Panier</a>
+    <div class="product-card__image">
+        <img src="{{ asset('images/livre-test.jpg') }}" alt="Livre Ouvert">
+    </div>
+    <div class="product-card__body">
+        <h3 class="product-card__title">Titre du Livre 1</h3>
+        <p class="product-card__price">€9.99</p>
+        <a href="#" class="button button--primary button__card">Ajouter au Panier</a>
+    </div>
 </article>
